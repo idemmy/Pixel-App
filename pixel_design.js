45fbf1da5d28b51a1c95da7eb49a1baba7488310
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2018-07-01 21:55:15 +0100

    initial commit

---
+++ pixel_design.js
@@ -0,0 +1,37 @@
+// Select color input
+// Select size input
+var height, width, color;
+
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    height = $('#input_height').val();
+    width = $('#input_width').val();
+    makeGrid(height, width);
+    // console.log('height: ' + height + ' and width: ' + width);
+})
+
+function makeGrid(x, y) {
+    $('tr').remove();
+
+// Your code goes here!
+for (var i =1; i <= x; i++) {
+    $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+    for (var j = 1; j <= y; j++) {
+        $('#table' + i).append('<td></td>');
+    }
+}
+
+//add color to cell
+$('td').click(function addColor() {
+    color = $('#colorPicker').val();
+
+    if ($(this).attr('style')) {
+        $(this).removeAttr('style')
+    } else {
+        $(this).attr('style', 'background-color:' + color);
+    }
+})
+}
+
+//psalm 77 and 82
